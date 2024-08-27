646eb236886ff1c994eae17fd6691e5b905ea944
Sekiju <zzzcxk@gmail.com>
2024-08-27 23:50:27 +0500

    remove: FolderReaderOptions.allowed_extensions

--- reline/nodes/folder_reader/node.py
+++ reline/nodes/folder_reader/node.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import os.path
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import List, Optional, Literal
 import logging
 
@@ -18,7 +18,6 @@ Mode = Literal['rgb', 'gray', 'dynamic']
 class FolderReaderOptions(NodeOptions):
     path: str
     recursive: Optional[bool] = False
-    allowed_extensions: Optional[List[str]] = field(default_factory=lambda: ['png', 'jpg', 'jpeg', 'webp'])
     mode: Optional[Mode] = 'dynamic'
 
 
@@ -26,10 +25,7 @@ class FolderReaderNode(Node[FolderReaderOptions]):
     def __init__(self, options: FolderReaderOptions):
         super().__init__(options)
         self.mode = MODE_MAP[options.mode]
-        self.allowed_extensions = options.allowed_extensions
         self.dir_path = os.path.abspath(self.options.path)
-        if isinstance(self.allowed_extensions, list):
-            self.allowed_extensions = [ext.lower() for ext in self.allowed_extensions]
 
     def _scandir(self, dir_path: str):
         file_paths = []
@@ -37,12 +33,7 @@ class FolderReaderNode(Node[FolderReaderOptions]):
         try:
             for entry in os.scandir(dir_path):
                 if entry.is_file():
-                    if isinstance(self.allowed_extensions, list):
-                        ext = entry.name.split('.')[-1].lower()
-                        if ext in self.options.allowed_extensions:
-                            file_paths.append(os.path.abspath(entry.path))
-                    else:
-                        file_paths.append(os.path.abspath(entry.path))
+                    file_paths.append(os.path.abspath(entry.path))
                 elif entry.is_dir() and self.options.recursive:
                     file_paths.extend(self._scandir(os.path.abspath(entry.path)))
         except OSError as e:
